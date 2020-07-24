e9f3bd893d2e7cd4725a67be2daf677bdd8a1e74
Shichen <86@qq.com>
2020-07-24 16:22:32 +0800

    是啥

---
+++ programer/js/index.js
@@ -0,0 +1,37 @@
+var pipCanvas = new PipCanvas({
+    imgList: [
+        {
+            link: 'img/cover_v1.jpg',
+            imgW: '750',
+            imgH: '1260',
+        },
+        {
+            link: 'img/p2.jpg',
+            imgW: '750',
+            imgH: '1260',
+            areaW: '58',
+            areaH: '99',
+            areaL: '601',
+            areaT: '783',
+        },
+        {
+            link: 'img/p3.jpg',
+            imgW: '750',
+            imgH: '1260',
+            areaW: '63',
+            areaH: '101',
+            areaL: '136',
+            areaT: '930',
+        },
+        {
+            link: 'img/p4.jpg',
+            imgW: '750',
+            imgH: '1260',
+            areaW: '80',
+            areaH: '134',
+            areaL: '335',
+            areaT: '1024',
+        }
+    ],
+    el: '#container',
+});
